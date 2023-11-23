4aac2884d7b812a4fe0f4726cf0b97d76df893f9
Hariskurnia <arieskurnia2@gmail.com>
2023-11-23 10:34:35 +0700

    'Sentrasolusiprimacss'

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,21 @@ module.exports = {
     },
     extend: {
       colors: {
-        primary: "#eecd17",
+        primary: "#017f9e",
         dark: "#0f172a",
+        "picton-blue": {
+          50: "#f1fafe",
+          100: "#e1f5fd",
+          200: "#bdebfa",
+          300: "#82dcf7",
+          400: "#3fcaf1",
+          500: "#20bde9",
+          600: "#0993c0",
+          700: "#09749b",
+          800: "#0c6380",
+          900: "#10516a",
+          950: "#0a3547",
+        },
       },
       screens: {
         "2xl": "1320px",
